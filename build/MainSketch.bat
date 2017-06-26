a8e03e726b6fa14a83f1b851d30a7cdd5494b7be
kawada <kawada@example.com>
2017-06-26 11:07:20 +0900

    (#22)アニメーションの動きごとにクラスを作成

---
+++ build/MainSketch.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem ƒJƒŒƒ“ƒgƒfƒBƒŒƒNƒgƒŠˆÚ“®
+cd /D %~dp0
+
+rem SketchÀs
+rem java -jar StartUp.jar com.awesomebase.processing.MainSketch3D
+
+rem y…‘°ŠÙz
+java -jar StartUp.jar com.awesomebase.processing.SketchAquarium
+
+rem y‰F’ˆz
+rem java -jar StartUp.jar com.awesomebase.processing.SketchSpace
+
+rem yU•àz
+rem java -jar StartUp.jar com.awesomebase.processing.SketchWalk
+
+exit /b 0
+
